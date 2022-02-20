f5cbac412b98ccccfa42237ac2e90fe5f8837dc7
a1ex_g@list.ru <Set1music>
2022-02-20 16:16:45 +0400

    start

---
+++ resources/js/components/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <router-link class="nav-link" :to="{ name: 'home'}">Главная</router-link>
+                    </li>
+                    <li class="nav-item active">
+                        <router-link class="nav-link" :to="{ name: 'desks'}">Доски</router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <p>
+
+        </p>
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
---
+++ resources/js/components/desks/Desks.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Доски</h1>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card mt-3">
+                    <a href="#" class="card-body">
+                        <h5 class="card-title">Заголовок карточки</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Desks",
+    data(){
+        return {
+            desks:[]
+        }
+    },
+    mounted() {
+        axios.get('/api/V1/desks')
+        .then(res=>{
+            console.log(res)
+        })
+    }
+}
+</script>
+
